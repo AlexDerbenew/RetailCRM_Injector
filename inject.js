111ff1086e3aaca9e4f4b62748e764f4dcf3d348
AlexDerbenew <alex.derbenew@yandex.ru>
2019-04-05 12:13:29 +0300

    Update in injection execution

--- inject.js
+++ inject.js
@@ -1,10 +1,13 @@
 'use strict';
 console.log("Injection file worked");
 
-document.addEventListener('ImgLinkEvent', function (e)
-{
-    var imgUrl=e.detail;
-    
+var imgUrl = undefined;
+
+function sleep (time){
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function appendUrls(){
     var orderIDs = Array();
     var elements = [].slice.call(document.getElementsByClassName('status-prop-list'));
     if(elements.length > 2){
@@ -30,10 +33,10 @@ document.addEventListener('ImgLinkEvent', function (e)
             var link = document.createElement('a');
             if(prop.getAttribute('title').includes('заказ') || prop.getAttribute('title').includes('поставка')){
                 var number = orderIDs[counter];
-                //link.setAttribute('href', 'https://yakosmetika.retailcrm.ru/orders/'+number+'/edit');
+                link.setAttribute('href', 'https://yakosmetika.retailcrm.ru/orders/'+number+'/edit');
                 link.setAttribute('display','inline-block');
                 link.setAttribute('style','float:left');
-                link.setAttribute('onclick', 'location.href="https://yakosmetika.retailcrm.ru/orders/'+number+'/edit"');
+                link.setAttribute('onclick', '(new Promise((resolve) => setTimeout(resolve, 2000))).then(()=>{document.dispatchEvent(new Event(\'OrderPageLoaded\'));})');
                 var linkImg = document.createElement('img');
                 linkImg.setAttribute('src', imgUrl);
                 linkImg.setAttribute('alt', 'Быстрый переход');
@@ -45,4 +48,35 @@ document.addEventListener('ImgLinkEvent', function (e)
             }
         });
     });
+}
+
+document.addEventListener('ImgLinkEvent', function (e){
+    console.log("Got extension img url " + e.detail);
+    imgUrl=e.detail;
+    console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+    var location = window.location.href;
+    location = location.split("/");
+    if(location.includes("orders")){
+        document.dispatchEvent(new Event('OrderPageLoaded'));
+    }
 });
+
+document.addEventListener('OrderPageLoaded', function(e){
+    console.log("PageLoaded event");
+    while(imgUrl==undefined){
+        sleep(100).then(()=>{});
+    }
+    sleep(2000).then(()=>{document.dispatchEvent(new Event('PageLoaded'));})
+    console.log("calling appendUrls");
+    appendUrls();
+});
+
+window.onpopstate = function(event) {
+    console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+    var location = window.location.href;
+    location = location.split("/");
+    if(location.includes("orders")){
+        document.dispatchEvent(new Event('OrderPageLoaded'));
+    }
+};
+
