128b9f13898362fa7782764fca89711d87932daa
AlexDerbenew <alex.derbenew@yandex.ru>
2019-05-05 20:33:00 +0300

    Final auto-injection version

--- background.js
+++ background.js
@@ -1,5 +1,36 @@
 'use strict';
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript(null, {file: "inject.js"});
+	var currentURL = tab.url;
+	if( currentURL.substr(0, 40) == "https://yakosmetika.retailcrm.ru/orders/" && currentURL.substr(-4, 4) == "edit" ){
+		chrome.tabs.executeScript(tab.id, {file: "inject.js"});
+	}
 });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+	var currentURL = tab.url;
+	chrome.extension.getBackgroundPage().console.log(changeInfo.status);
+	chrome.extension.getBackgroundPage().console.log(currentURL);
+	if( currentURL.substr(0, 40) == "https://yakosmetika.retailcrm.ru/orders/" && currentURL.substr(-4, 4) == "edit" && changeInfo.status == "complete"){
+		chrome.tabs.executeScript(tabId, {file: "inject.js"});
+	}
+});
+
+
+// var oldURL = window.location.href;
+// function checkURLchange(currentURL){
+// 	var currentURL = window.location.href;
+//     if(currentURL != oldURL){
+//         console.log("url changed!");
+// 		oldURL = currentURL;
+// 		if( currentURL.substr(0, 40) == "https://yakosmetika.retailcrm.ru/orders/" && currentURL.substr(-4, 4) == "edit" ){
+// 			chrome.tabs.executeScript(null, {file: "inject.js"});
+// 		}
+//     }
+// 	console.log("TEST");
+//     oldURL = window.location.href;
+//     setTimeout(function() {
+//         checkURLchange(window.location.href);
+//     }.bind(this), 3000);
+// }
+
+// checkURLchange(currentURL);
